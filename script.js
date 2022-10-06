4d41c1fad9a6144fd0dfb55bccb011ce8b66b081
Felipe Fernandes <felipe.tavares.fernandes@gmail.com>
2022-10-06 09:20:59 -0300

    requirements 7 and 8 done

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const todoList = document.getElementById('lista-tarefas');
 const button = document.getElementById('criar-tarefa');
 
 function newTask() {
-  const task = document.createElement('li');
-  console.log(input.value);
-  task.innerText = input.value;
-  todoList.appendChild(task);
-  input.value = '';
+  if (input.value !== '') {
+    const task = document.createElement('li');
+    task.innerText = input.value;
+    todoList.appendChild(task);
+    input.value = '';
+  }
+}
+
+function selectTask() {
+  const task = window.event.target;
+  const selected = document.getElementsByClassName('selected')[0];
+  if (selected != null) {
+    selected.removeAttribute('class');
+  }
+  task.className = 'selected';
 }
 
 button.addEventListener('click', newTask);
+todoList.addEventListener('click', selectTask);
