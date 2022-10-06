d4326da9b1b6d09e9fa9e3c84f2818afb8eff89f
Felipe Fernandes <felipe.tavares.fernandes@gmail.com>
2022-10-06 18:58:26 -0300

    requirements 12 and 14 done

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const todoList = document.getElementById('lista-tarefas');
 const buttonNewTask = document.getElementById('criar-tarefa');
 const buttonClearList = document.getElementById('apaga-tudo');
 const buttonClearDones = document.getElementById('remover-finalizados');
+const buttonSaveList = document.getElementById('salvar-tarefas');
+const buttonUp = document.getElementById('mover-cima');
+const buttonLow = document.getElementById('mover-baixo');
+const buttonRemove = document.getElementById('remover-selecionado');
+
+function saveList() {
+  localStorage.setItem('savedList', todoList.innerHTML);
+}
+
+function loadList() {
+  if (localStorage.getItem('savedList')) {
+    todoList.innerHTML = localStorage.getItem('savedList');
+  }
+}
 
 function newTask() {
   if (input.value !== '') {
@@ -11,33 +25,38 @@ function newTask() {
     todoList.appendChild(task);
     input.value = '';
   }
+  saveList();
 }
 
 function selectTask() {
   const task = window.event.target;
   const selected = document.getElementsByClassName('selected')[0];
-  if (selected != null) {
-    selected.removeAttribute('class');
+  if (task.className.includes('selected')) {
+    task.classList.remove('selected');
+  } else {
+    if (selected != null) {
+      selected.classList.remove('selected');
+    }
+    task.classList.add('selected');
   }
-  if (task.className !== 'completed') {
-    task.className = 'selected';
-  }
+  saveList();
 }
 
 function completedTask() {
   const task = window.event.target;
-  console.log(task);
-  if (task.className === 'completed') {
-    task.removeAttribute('class');
+  if (task.className.includes('completed')) {
+    task.classList.remove('completed');
   } else {
-    task.className = 'completed';
+    task.classList.add('completed');
   }
+  saveList();
 }
 
 function clearList() {
   while (todoList.firstChild) {
     todoList.removeChild(todoList.firstChild);
   }
+  saveList();
 }
 
 function clearDones() {
@@ -45,10 +64,46 @@ function clearDones() {
   for (let index = completed.length - 1; index >= 0; index -= 1) {
     todoList.removeChild(completed[index]);
   }
+  saveList();
 }
+
+function taskUp() {
+  const selected = document.getElementsByClassName('selected')[0];
+  if (selected && selected.previousElementSibling !== null) {
+    const selectedHTML = selected.outerHTML;
+    const previousElementHTML = selected.previousElementSibling.outerHTML;
+    selected.previousElementSibling.outerHTML = selectedHTML;
+    selected.outerHTML = previousElementHTML;
+    saveList();
+  }
+}
+
+function taskLow() {
+  const selected = document.getElementsByClassName('selected')[0];
+  if (selected && selected.nextElementSibling !== null) {
+    const selectedHTML = selected.outerHTML;
+    const nextElementHTML = selected.nextElementSibling.outerHTML;
+    selected.nextElementSibling.outerHTML = selectedHTML;
+    selected.outerHTML = nextElementHTML;
+    saveList();
+  }
+}
+
+function removeSelected() {
+  const selected = document.getElementsByClassName('selected')[0];
+  if (selected) {
+    selected.outerHTML = '';
+  }
+}
+
+loadList();
 
 buttonNewTask.addEventListener('click', newTask);
 buttonClearList.addEventListener('click', clearList);
 buttonClearDones.addEventListener('click', clearDones);
+buttonSaveList.addEventListener('click', saveList);
+buttonUp.addEventListener('click', taskUp);
+buttonLow.addEventListener('click', taskLow);
+buttonRemove.addEventListener('click', removeSelected);
 todoList.addEventListener('click', selectTask);
 todoList.addEventListener('dblclick', completedTask);
